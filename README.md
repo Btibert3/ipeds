b063b8a88b55972c3743089fe348e66f0a0eb05a
Brock Tibert <btibert3@gmail.com>
2018-10-19 17:49:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+
+This python package is aimed at interfacing with the IPEDS surveys for higher
+education.  I may add other functions later to round out data needs within a
+python package if one is inclined to study/explore/crawl US higher education
+data.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ipeds',
+      version='0.1',
+      description='Interface with IPEDS and other US higher ed datasets',
+      url='http://github.com/btibert3/ipeds',
+      author='Brock Tibert',
+      author_email='btibert3@gmail.com',
+      license='MIT',
+      packages=['ipeds'],
+      zip_safe=False)
